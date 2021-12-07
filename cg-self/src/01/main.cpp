53763e938a781290677c36b1c2e48b9a9b655f81
Christopher Rose <christophervrose@gmail.com>
2021-12-08 02:20:58 +0530

    [add] 1

--- cg-self/src/01/main.cpp
+++ cg-self/src/01/main.cpp
@@ -1,7 +1,7 @@
 #include <GL/glut.h>
 #include <iostream>
 
-void draw_line(int x1, int x2, int y1, int y2,float color[3])
+void draw_line(int x1, int x2, int y1, int y2, float color[3])
 {
     int dx, dy, i, e, incx, incy, inc1, inc2, x, y;
 
@@ -24,59 +24,59 @@ void draw_line(int x1, int x2, int y1, int y2,float color[3])
     glColor3fv(color);
     glBegin(GL_POINTS);
     if (dx == 0) {
-        if (dy==0) {
-            glVertex2d(x1,y1);
+        if (dy == 0) {
+            glVertex2d(x1, y1);
         } else if (y1 > y2) {
             for (int i = y2; i <= y1; i++) {
-                glVertex2i(x1,i);
+                glVertex2i(x1, i);
             }
         } else {
             // y2>y1
-            for(int i=y1;i<=y2;i++){
-                glVertex2i(x1,i);
+            for (int i = y1; i <= y2; i++) {
+                glVertex2i(x1, i);
             }
         }
     } else if (dy == 0) {
         if (x1 > x2) {
             for (int i = x2; i <= x1; i++) {
-                glVertex2i(i,y1);
+                glVertex2i(i, y1);
             }
         } else {
             // y2>y1
-            for(int i=x1;i<=x2;i++){
-                glVertex2i(i,y1);
+            for (int i = x1; i <= x2; i++) {
+                glVertex2i(i, y1);
             }
         }
     } else if (dx > dy) {
-        glVertex2i(x,y);
-        e = 2*dx-dy;
-        inc1 = 2*(dx-dy);
-        inc2 = 2*dx;
-        for(i=0;i<dy;i++){
-            if(e>=0){
-                x+=incx;
-                e+=inc1;
-            }else{
-                e+=inc2;
+        glVertex2i(x, y);
+        e = 2 * dy - dy;
+        inc1 = 2 * (dy - dx);
+        inc2 = 2 * dy;
+        for (i = 0; i < dx; i++) {
+            if (e >= 0) {
+                y += incy;
+                e += inc1;
+            } else {
+                e += inc2;
             }
-            y+=incy;
-            glVertex2i(x,y);
+            x += incx;
+            glVertex2i(x, y);
         }
     } else {
 
-        glVertex2i(x,y);
-        e = 2*dy-dx;
-        inc1 = 2*(dy-dx);
-        inc2 = 2*dy;
-        for(i=0;i<dx;i++){
-            if(e>=0){
-                y+=incy;
-                e+=inc1;
-            }else{
-                e+=inc2;
+        glVertex2i(x, y);
+        e = 2 * dx - dy;
+        inc1 = 2 * (dx - dy);
+        inc2 = 2 * dx;
+        for (i = 0; i < dy; i++) {
+            if (e >= 0) {
+                x += incx;
+                e += inc1;
+            } else {
+                e += inc2;
             }
-            x+=incx;
-            glVertex2i(x,y);
+            y += incy;
+            glVertex2i(x, y);
         }
     }
 
@@ -85,7 +85,7 @@ void draw_line(int x1, int x2, int y1, int y2,float color[3])
 }
 
 // int x1,x2,y1,y2;
-float color[3]={1,1,1};
+float color[3] = { 1, 1, 1 };
 
 void displayLoop()
 {
@@ -93,16 +93,36 @@ void displayLoop()
     // draw_line(x1,x2,y1,y2,color);
 }
 
-
-
-
 void myinit()
 {
     glClearColor(0, 0, 0, 1);
-
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0, 400, 0, 400, -10, 10);
+    glMatrixMode(GL_MODELVIEW);
     return;
 }
-
+int x1c, x2c, y1c, y2c;
+void mouse(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        x1c = x;
+        y1c = 400 - y;
+    } else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+        x2c = x;
+        y2c = 400 - y;
+        draw_line(x1c, x2c, y1c, y2c, color);
+        printf("(%d,%d)->(%d,%d)\n",x1c,y1c,x2c,y2c);
+    }
+    glutPostRedisplay();
+}
+void keeb(u_char key, int x, int y)
+{
+    if (key == 'c') {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glFlush();
+        glutPostRedisplay();
+    }
+}
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -119,7 +139,8 @@ int main(int argc, char** argv)
     // std::cin>>x2>>y2;
 
     glutDisplayFunc(displayLoop);
-
+    glutMouseFunc(mouse);
+    glutKeyboardFunc(keeb);
     glutMainLoop();
     return 0;
 }
