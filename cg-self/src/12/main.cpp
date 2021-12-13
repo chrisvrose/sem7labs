c3e259227ce1fc68d041110833ee3f5484381efe
Christopher Rose <christophervrose@gmail.com>
2021-12-13 19:53:48 +0530

    [add] 12

--- cg-self/src/12/main.cpp
+++ cg-self/src/12/main.cpp
@@ -1,74 +1,71 @@
-#include<GL/glut.h>
+#include <GL/glut.h>
 
-float vertices[8][3] = {
-    {0,0,0},
-    {250,0,0},
-    {250,250,0},
-    {0,250,0},
-    {0,250,250},
-    {250,250,250},
-    {250,0,250},
-    {0,0,250}
+float vertices[8][3] = {0,   0,   0,
+
+                        250, 0,   0,
+
+                        250, 250, 0,
+
+                        0,   250, 0,
+
+                        0,   250, 250,
+
+                        250, 250, 250,
+
+                        250, 0,   250,
+
+                        0,   0,   250};
+
+float colors[8][3] = {0, 0, 0,
+
+                        1, 0, 0,
+
+                        1, 1, 0,
+
+                        0, 1, 0,
+
+                        0, 1, 1,
+
+                        1, 1, 1,
+
+                        1, 0, 1,
+
+                        0, 0, 1};
+
+// use GLubyte **OR** unsigned char
+GLubyte elementindices[24] = {
+    0,1,2,3,
+
+    0,3,4,7,
+
+    0,7,6,1,
+
+    3,4,5,2,
+
+    2,5,6,1,
+
+    7,4,5,6
 };
 
-inline void drawCube(){
-
-
-    glColor3f(1,1,1);
-
-    glBegin(GL_QUADS);
-    for(int i=0;i<4;i++)
-    glVertex3fv(vertices[i]);
-    glEnd();
-
-    glColor3f(1,0,0);
-    glBegin(GL_QUADS);
-    glVertex3fv(vertices[0]);
-    glVertex3fv(vertices[3]);
-    glVertex3fv(vertices[4]);
-    glVertex3fv(vertices[7]);
-    glEnd();
-
-    glColor3f(1,1,0);
-    glBegin(GL_QUADS);
-    glVertex3fv(vertices[0]);
-    glVertex3fv(vertices[7]);
-    glVertex3fv(vertices[6]);
-    glVertex3fv(vertices[1]);
-    glEnd();
-
-
-    glColor3f(0,1,0);
-    glBegin(GL_QUADS);
-    glVertex3fv(vertices[3]);
-    glVertex3fv(vertices[4]);
-    glVertex3fv(vertices[5]);
-    glVertex3fv(vertices[2]);
-    glEnd();
-
-
-    glColor3f(0,1,1);
-    glBegin(GL_QUADS);
-    glVertex3fv(vertices[2]);
-    glVertex3fv(vertices[5]);
-    glVertex3fv(vertices[6]);
-    glVertex3fv(vertices[1]);
-    glEnd();
-
-    glColor3f(1,0,1);
-    glBegin(GL_QUADS);
-    glVertex3fv(vertices[7]);
-    glVertex3fv(vertices[4]);
-    glVertex3fv(vertices[5]);
-    glVertex3fv(vertices[6]);
-    glEnd();
-
+inline void drawCube() {
+    /*
+     * This will draw with the help of the bound vertex and color (and normal) pointers.
+     * Pass a index list and it makes a drawcall with that-th element
+     * 
+     * Eg. {1,3} will issue a draw for the 2nd and 4th draw call 
+     * Args:
+     * 1. Type of draw calls
+     * 
+     * 2. Number of draw calls present in the index list
+     * 3. What is the data type of the index list elements (ubyte array here)
+     * 4. index list
+     */
+    glDrawElements(GL_QUADS,24,GL_UNSIGNED_BYTE,elementindices);
     glFlush();
-
 }
 
-void disp(){
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+void disp() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     drawCube();
 
@@ -76,71 +73,56 @@ void disp(){
     glutSwapBuffers();
 }
 
-void idle(){
-    glRotated(.1,1,0,0);
-    glRotated(.2,0,1,0);
-    glRotated(.3,0,0,1);
+void idle() {
+    // bruh just rotate
+    glRotated(.1, 1, 0, 0);
+    glRotated(.2, 0, 1, 0);
+    glRotated(.3, 0, 0, 1);
     glutPostRedisplay();
 }
 
-void myinit(){
+void myinit() {
     glEnable(GL_DEPTH_TEST);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(90,1,.1,2000.f);
-    glMatrixMode(GL_MODELVIEW);
+    // perspective op
+    // just pass fov, aspect ratio (just keep it 1 if square), and near and far
+    gluPerspective(90, 1, .1, 2000.f);
 
-    glTranslated(-100,0,-800);
 
-}
-// attach this to explore
-void keeb(unsigned char k, int x,int y){
+
 
     glMatrixMode(GL_MODELVIEW);
-
-    
-    switch(k){
-        case 'r':
-            glLoadIdentity();
-            glTranslated(-100,0,-800);
-            break;
-        case ' ':
-            glTranslated(0,-10,0);
-            break;
-        case 'x':
-            glTranslated(0,10,0);
-            break;
-        case 'w':
-            glTranslated(0,0,10);
-            break;
-
-        case 's':
-            glTranslated(0,0,-10);
-            break;
-
-        case 'a':
-            // glTranslated(0,0,10);
-            glRotatef(10,0,1,0);
-            break;
-        case 'd':
-            glRotatef(-10,0,1,0);
-
-            // glTranslated(0,0,10);
-            break;
-    }
-    glutPostRedisplay();
+    // move the cube a bit away
+    glTranslated(0, 0, -800);
 }
 
-int main(int argc,char** argv){
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_DEPTH|GLUT_RGBA|GLUT_DOUBLE);
-    glutInitWindowSize(500,500);
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
 
     glutCreateWindow("No");
     glutDisplayFunc(disp);
-    glutKeyboardFunc(keeb);
+    
     glutIdleFunc(idle);
+
+    // we can use vertex and color arrays and pass them to opengl
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    /*
+     * Explaining this - We are passing a set of triplets to OpenGL 
+     * 1. Size of the ntuple - 3 -> x, y and z coordinate
+     * 2. Datatype
+     * 3. Stride - The gap between two successive elements - *3* elements, or *0* for OpenGL to autodetect it
+     *  (Stride is important if your array contains other data that you're passing at once)
+     * 4. The actual data
+     */
+    glVertexPointer(3,GL_FLOAT,3,vertices);
+    glColorPointer(3,GL_FLOAT,3,colors);
+
     myinit();
 
     glutMainLoop();
