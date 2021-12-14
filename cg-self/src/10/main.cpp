ec6c745fd76a9e489ccb5b8dc136d4304aacb37f
Christopher Rose <christophervrose@gmail.com>
2021-12-14 21:26:51 +0530

    [fix]

--- cg-self/src/10/main.cpp
+++ cg-self/src/10/main.cpp
@@ -1,122 +1,183 @@
-#include <GL/glut.h>
-#include <stdio.h>
+// C++ program for implementing Sutherlandï¿½Hodgman 
+// algorithm for polygon clipping 
+#include<iostream>
+#include<GL/glut.h>
+using namespace std;
+int poly_size, poly_points[20][2], org_poly_size, org_poly_points[20][2], clipper_size, clipper_points[20][2];
+const int MAX_POINTS = 20;
 
-double xmin = 50, ymin = 50, xmax = 100, ymax = 100;  // Window boundaries
-double xvmin = 200, yvmin = 200, xvmax = 300,
-       yvmax = 300;  // Viewport boundaries
 
-int cliptest(double p, double q, double* u1, double* u2) {
-    double r;
-    if (p)
-        r = q / p;  // to check whether p
-    if (p < 0.0)    // potentially entry point, update te
-    {
-        if (r > *u1)
-            *u1 = r;
-        if (r > *u2)
-            return (false);  // line portion is outside
-    } else if (p > 0.0)      //  Potentially leaving point, update tl
-    {
-        if (r < *u2)
-            *u2 = r;
-        if (r < *u1)
-            return (false);  // line portion is outside
-    } else if (p == 0.0) {
-        if (q < 0.0)
-            return (false);  // line parallel to edge but outside
-    }
-    return (true);
+// Returns x-value of point of intersection of two 
+// lines 
+
+void drawPoly(int p[][2], int n) {
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < n; i++)
+		glVertex2f(p[i][0], p[i][1]);
+	glEnd();
 }
 
-void LiangBarskyLineClipAndDraw(double x0, double y0, double x1, double y1) {
-    double dx = x1 - x0, dy = y1 - y0, u1 = 0.0, u2 = 1.0;
-
-    // draw a red colored viewport
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(xvmin, yvmin);
-    glVertex2f(xvmax, yvmin);
-    glVertex2f(xvmax, yvmax);
-    glVertex2f(xvmin, yvmax);
-    glEnd();
-
-    if (cliptest(-dx, x0 - xmin, &u1, &u2))     // inside test wrt left edge
-        if (cliptest(dx, xmax - x0, &u1, &u2))  // inside test wrt right edge
-            if (cliptest(-dy, y0 - ymin, &u1,
-                         &u2))  // inside test wrt bottom edge
-                if (cliptest(dy, ymax - y0, &u1,
-                             &u2))  // inside test wrt top edge
-                {
-                    if (u2 < 1.0) {
-                        x1 = x0 + u2 * dx;
-                        y1 = y0 + u2 * dy;
-                    }
-
-                    if (u1 > 0.0) {
-                        x0 = x0 + u1 * dx;
-                        y0 = y0 + u1 * dy;
-                    }
-                    // Window to viewport mappings
-                    double sx =
-                        (xvmax - xvmin) / (xmax - xmin);  // Scale parameters
-                    double sy = (yvmax - yvmin) / (ymax - ymin);
-                    double vx0 = xvmin + (x0 - xmin) * sx;
-                    double vy0 = yvmin + (y0 - ymin) * sy;
-                    double vx1 = xvmin + (x1 - xmin) * sx;
-                    double vy1 = yvmin + (y1 - ymin) * sy;
-
-                    glColor3f(0.0, 0.0, 1.0);  // draw blue colored clipped line
-                    glBegin(GL_LINES);
-                    glVertex2d(vx0, vy0);
-                    glVertex2d(vx1, vy1);
-                    glEnd();
-                }
-}  // end of line clipping
-void display() {
-    double x0 = 60, y0 = 60, x1 = 90, y1 = 90;
-    double p0 = 110, q0 = 30, p1 = 30, q1 = 120;
-    glClear(GL_COLOR_BUFFER_BIT);
-    // draw the line with red color
-    glColor3f(1.0, 0.0, 0.0);
-
-    glBegin(GL_LINES);
-    glVertex2d(x0, y0);
-    glVertex2d(x1, y1);
-
-    glVertex2d(p0, q0);
-    glVertex2d(p1, q1);
-
-    glEnd();
-
-    // draw a blue colored window
-    glColor3f(0.0, 0.0, 1.0);
-
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(xmin, ymin);
-    glVertex2f(xmax, ymin);
-    glVertex2f(xmax, ymax);
-    glVertex2f(xmin, ymax);
-    glEnd();
-    LiangBarskyLineClipAndDraw(x0, y0, x1, y1);
-    LiangBarskyLineClipAndDraw(p0, q0, p1, q1);
-
-    glFlush();
+int x_intersect(int x1, int y1, int x2, int y2,
+	int x3, int y3, int x4, int y4)
+{
+	int num = (x1 * y2 - y1 * x2) * (x3 - x4) -
+		(x1 - x2) * (x3 * y4 - y3 * x4);
+	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
 }
-void myinit() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glColor3f(1.0, 0.0, 0.0);
-    glLineWidth(2.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+
+// Returns y-value of point of intersectipn of 
+// two lines 
+int y_intersect(int x1, int y1, int x2, int y2,
+	int x3, int y3, int x4, int y4)
+{
+	int num = (x1 * y2 - y1 * x2) * (y3 - y4) -
+		(y1 - y2) * (x3 * y4 - y3 * x4);
+	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
 }
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("Liang Barsky Line Clipping Algorithm");
-    glutDisplayFunc(display);
-    myinit();
-    glutMainLoop();
+
+// This functions clips all the edges w.r.t one clip 
+// edge of clipping area 
+void clip(int poly_points[][2], int& poly_size,
+	int x1, int y1, int x2, int y2)
+{
+	int new_points[MAX_POINTS][2], new_poly_size = 0;
+
+	// (ix,iy),(kx,ky) are the co-ordinate values of 
+	// the points 
+	for (int i = 0; i < poly_size; i++)
+	{
+		// i and k form a line in polygon 
+		int k = (i + 1) % poly_size;
+		int ix = poly_points[i][0], iy = poly_points[i][1];
+		int kx = poly_points[k][0], ky = poly_points[k][1];
+
+		// Calculating position of first point 
+		// w.r.t. clipper line 
+		int i_pos = (x2 - x1) * (iy - y1) - (y2 - y1) * (ix - x1);
+
+		// Calculating position of second point 
+		// w.r.t. clipper line 
+		int k_pos = (x2 - x1) * (ky - y1) - (y2 - y1) * (kx - x1);
+
+		// Case 1 : When both points are inside 
+		if (i_pos >= 0 && k_pos >= 0)
+		{
+			//Only second point is added 
+			new_points[new_poly_size][0] = kx;
+			new_points[new_poly_size][1] = ky;
+			new_poly_size++;
+		}
+
+		// Case 2: When only first point is outside 
+		else if (i_pos < 0 && k_pos >= 0)
+		{
+			// Point of intersection with edge 
+			// and the second point is added 
+			new_points[new_poly_size][0] = x_intersect(x1,
+				y1, x2, y2, ix, iy, kx, ky);
+			new_points[new_poly_size][1] = y_intersect(x1,
+				y1, x2, y2, ix, iy, kx, ky);
+			new_poly_size++;
+
+			new_points[new_poly_size][0] = kx;
+			new_points[new_poly_size][1] = ky;
+			new_poly_size++;
+		}
+
+		// Case 3: When only second point is outside 
+		else if (i_pos >= 0 && k_pos < 0)
+		{
+			//Only point of intersection with edge is added 
+			new_points[new_poly_size][0] = x_intersect(x1,
+				y1, x2, y2, ix, iy, kx, ky);
+			new_points[new_poly_size][1] = y_intersect(x1,
+				y1, x2, y2, ix, iy, kx, ky);
+			new_poly_size++;
+		}
+
+		// Case 4: When both points are outside 
+		else
+		{
+			//No points are added 
+		}
+	}
+
+	// Copying new points into original array 
+	// and changing the no. of vertices 
+	poly_size = new_poly_size;
+	for (int i = 0; i < poly_size; i++)
+	{
+		poly_points[i][0] = new_points[i][0];
+		poly_points[i][1] = new_points[i][1];
+	}
+}
+
+
+void init() {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 500.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+// Implements Sutherlandï¿½Hodgman algorithm 
+void display()
+{
+	init();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	drawPoly(clipper_points, clipper_size);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	drawPoly(org_poly_points, org_poly_size);
+	//i and k are two consecutive indexes 
+
+	for (int i = 0; i < clipper_size; i++)
+	{
+		int k = (i + 1) % clipper_size;
+
+		// We pass the current array of vertices, it's size 
+		// and the end points of the selected clipper line 
+		clip(poly_points, poly_size, clipper_points[i][0],
+			clipper_points[i][1], clipper_points[k][0],
+			clipper_points[k][1]);
+	}
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	drawPoly(poly_points, poly_size);
+	glFlush();
+}
+
+//Driver code 
+int main(int argc, char* argv[])
+{
+	printf("Enter no. of vertices: \n");
+	scanf_s("%d", &poly_size);
+	org_poly_size = poly_size;
+	for (int i = 0; i < poly_size; i++)
+	{
+		printf("Polygon Vertex:\n");
+		scanf_s("%d%d", &poly_points[i][0], &poly_points[i][1]);
+		org_poly_points[i][0] = poly_points[i][0];
+		org_poly_points[i][1] = poly_points[i][1];
+	}
+
+	printf("Enter no. of vertices of clipping window:");
+	scanf_s("%d", &clipper_size);
+	for (int i = 0; i < clipper_size; i++)
+	{
+		printf("Clip Vertex:\n");
+		scanf_s("%d%d", &clipper_points[i][0], &clipper_points[i][1]);
+	}
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Polygon Clipping!");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
 }
