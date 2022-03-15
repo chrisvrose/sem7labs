24e58a701d36b26c17cfe9182a97d8c2e57e0f16
Christopher Rose <christophervrose@gmail.com>
2022-03-15 16:47:15 +0530

    i want to cry

--- cg-f/src/10/main.cpp
+++ cg-f/src/10/main.cpp
@@ -1,183 +1,186 @@
-// C++ program for implementing Sutherlandï¿½Hodgman 
-// algorithm for polygon clipping 
-#include<iostream>
-#include<GL/glut.h>
+// sutherland hodgeman
+//  #include<iostream>
+#include <GL/glut.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-int poly_size, poly_points[20][2], org_poly_size, org_poly_points[20][2], clipper_size, clipper_points[20][2];
-const int MAX_POINTS = 20;
+typedef struct {float x;float y;} vec2;
+vector<vec2> poly;
+vector<vec2> clipper_ends;
 
-
-// Returns x-value of point of intersection of two 
-// lines 
-
-void drawPoly(int p[][2], int n) {
-	glBegin(GL_POLYGON);
-	for (int i = 0; i < n; i++)
-		glVertex2f(p[i][0], p[i][1]);
-	glEnd();
-}
-
-int x_intersect(int x1, int y1, int x2, int y2,
-	int x3, int y3, int x4, int y4)
-{
-	int num = (x1 * y2 - y1 * x2) * (x3 - x4) -
-		(x1 - x2) * (x3 * y4 - y3 * x4);
-	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	return num / den;
-}
-
-// Returns y-value of point of intersectipn of 
-// two lines 
-int y_intersect(int x1, int y1, int x2, int y2,
-	int x3, int y3, int x4, int y4)
-{
-	int num = (x1 * y2 - y1 * x2) * (y3 - y4) -
-		(y1 - y2) * (x3 * y4 - y3 * x4);
-	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	return num / den;
-}
-
-// This functions clips all the edges w.r.t one clip 
-// edge of clipping area 
-void clip(int poly_points[][2], int& poly_size,
-	int x1, int y1, int x2, int y2)
-{
-	int new_points[MAX_POINTS][2], new_poly_size = 0;
-
-	// (ix,iy),(kx,ky) are the co-ordinate values of 
-	// the points 
-	for (int i = 0; i < poly_size; i++)
-	{
-		// i and k form a line in polygon 
-		int k = (i + 1) % poly_size;
-		int ix = poly_points[i][0], iy = poly_points[i][1];
-		int kx = poly_points[k][0], ky = poly_points[k][1];
-
-		// Calculating position of first point 
-		// w.r.t. clipper line 
-		int i_pos = (x2 - x1) * (iy - y1) - (y2 - y1) * (ix - x1);
-
-		// Calculating position of second point 
-		// w.r.t. clipper line 
-		int k_pos = (x2 - x1) * (ky - y1) - (y2 - y1) * (kx - x1);
-
-		// Case 1 : When both points are inside 
-		if (i_pos >= 0 && k_pos >= 0)
-		{
-			//Only second point is added 
-			new_points[new_poly_size][0] = kx;
-			new_points[new_poly_size][1] = ky;
-			new_poly_size++;
-		}
-
-		// Case 2: When only first point is outside 
-		else if (i_pos < 0 && k_pos >= 0)
-		{
-			// Point of intersection with edge 
-			// and the second point is added 
-			new_points[new_poly_size][0] = x_intersect(x1,
-				y1, x2, y2, ix, iy, kx, ky);
-			new_points[new_poly_size][1] = y_intersect(x1,
-				y1, x2, y2, ix, iy, kx, ky);
-			new_poly_size++;
-
-			new_points[new_poly_size][0] = kx;
-			new_points[new_poly_size][1] = ky;
-			new_poly_size++;
-		}
-
-		// Case 3: When only second point is outside 
-		else if (i_pos >= 0 && k_pos < 0)
-		{
-			//Only point of intersection with edge is added 
-			new_points[new_poly_size][0] = x_intersect(x1,
-				y1, x2, y2, ix, iy, kx, ky);
-			new_points[new_poly_size][1] = y_intersect(x1,
-				y1, x2, y2, ix, iy, kx, ky);
-			new_poly_size++;
-		}
-
-		// Case 4: When both points are outside 
-		else
-		{
-			//No points are added 
-		}
-	}
-
-	// Copying new points into original array 
-	// and changing the no. of vertices 
-	poly_size = new_poly_size;
-	for (int i = 0; i < poly_size; i++)
-	{
-		poly_points[i][0] = new_points[i][0];
-		poly_points[i][1] = new_points[i][1];
-	}
-}
-
+int ymin, ymax, xmin, xmax;
 
 void init() {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 500.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 499, 0.0, 499);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
-// Implements Sutherlandï¿½Hodgman algorithm 
-void display()
-{
-	init();
-	glColor3f(1.0f, 0.0f, 0.0f);
-	drawPoly(clipper_points, clipper_size);
-
-	glColor3f(0.0f, 1.0f, 0.0f);
-	drawPoly(org_poly_points, org_poly_size);
-	//i and k are two consecutive indexes 
-
-	for (int i = 0; i < clipper_size; i++)
-	{
-		int k = (i + 1) % clipper_size;
-
-		// We pass the current array of vertices, it's size 
-		// and the end points of the selected clipper line 
-		clip(poly_points, poly_size, clipper_points[i][0],
-			clipper_points[i][1], clipper_points[k][0],
-			clipper_points[k][1]);
-	}
-
-	glColor3f(0.0f, 0.0f, 1.0f);
-	drawPoly(poly_points, poly_size);
-	glFlush();
+void drawPoly(vector<vec2> poly) {
+    glBegin(GL_POLYGON);
+    for (auto v : poly) {
+        glVertex2f(v.x,v.y);
+    }
+    glEnd();
 }
 
-//Driver code 
-int main(int argc, char* argv[])
-{
-	printf("Enter no. of vertices: \n");
-	scanf("%d", &poly_size);
-	org_poly_size = poly_size;
-	for (int i = 0; i < poly_size; i++)
-	{
-		printf("Polygon Vertex:\n");
-		scanf("%d%d", &poly_points[i][0], &poly_points[i][1]);
-		org_poly_points[i][0] = poly_points[i][0];
-		org_poly_points[i][1] = poly_points[i][1];
-	}
+void clip(vector<vec2> mypoly) {
+    vector<vec2> newpoints;
+    int n = mypoly.size();
+    
+    //Top
+    for (int i = 0; i < n; i++) {
+        int inext = (i + 1) % n;
 
-	printf("Enter no. of vertices of clipping window:");
-	scanf("%d", &clipper_size);
-	for (int i = 0; i < clipper_size; i++)
-	{
-		printf("Clip Vertex:\n");
-		scanf("%d%d", &clipper_points[i][0], &clipper_points[i][1]);
-	}
+        vec2& current = mypoly[i];
+        vec2& next = mypoly[inext];
+        bool isFirstInside = current.y<=ymax,isSecondIn= next.y<=ymax;
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(400, 400);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Polygon Clipping!");
-	glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+        if(isFirstInside&&isSecondIn){ //in->in
+            newpoints.push_back(next);
+        }else if(!isFirstInside && isSecondIn){ // out->in
+            // this inpoint is done the same way as cohen sutherland clips a line
+            vec2 inpoint = {current.x+(ymax-current.y)*(next.x-current.x)/(next.y-current.y),ymax};  // x0=x0+ ymax-y0 * x1-x0
+            newpoints.push_back(inpoint);
+            
+            newpoints.push_back(next);
+        }else if(isFirstInside && !isSecondIn){ // in->out
+            vec2 inpoint = {current.x+(ymax-current.y)*(next.x-current.x)/(next.y-current.y),ymax};  // x0=x0+ ymax-y1 * x1-x0
+            newpoints.push_back(inpoint);
+        }
+    }
+    swap(newpoints,mypoly);newpoints.clear();//"move" mypoly = newpoints, clear newpoints
+
+
+    //bottom 
+    n = mypoly.size();
+    for(int i=0;i<n;i++){
+        vec2 current = mypoly[i];
+        vec2 next = mypoly[(i+1)%n];
+
+        bool isFirstInside = current.y>=ymin,isSecondIn = next.y>=ymin;
+
+        if(isFirstInside&&isSecondIn){ //in->in
+            newpoints.push_back(next);
+        }else if(!isFirstInside && isSecondIn){ // out->in
+            vec2 inpoint = {current.x+(ymin-current.y)*(next.x-current.x)/(next.y-current.y),ymin};  // x0=x0+ ymax-y0 * x1-x0
+            newpoints.push_back(inpoint);
+            
+            newpoints.push_back(next);
+        }else if(isFirstInside && !isSecondIn){ // in->out
+            vec2 inpoint = {current.x+(ymin-current.y)*(next.x-current.x)/(next.y-current.y),ymin};  // x0=x0+ ymax-y1 * x1-x0
+            newpoints.push_back(inpoint);
+        }else{
+            cout<<"Completely out";
+        }
+    }
+    swap(newpoints,mypoly);newpoints.clear();//"move" mypoly = newpoints, clear newpoints
+
+    //r
+    n = mypoly.size();
+
+    for(int i=0;i<n;i++){
+        vec2 current = mypoly[i];
+        vec2 next = mypoly[(i+1)%n];
+
+        bool isFirstInside = current.x<=xmax,isSecondIn = next.x<=xmax;
+
+        if(isFirstInside&&isSecondIn){ //in->in
+            newpoints.push_back(next);
+        }else if(!isFirstInside && isSecondIn){ // out->in
+            vec2 inpoint = {xmax,current.y+(xmax-current.x)*(next.y-current.y)/(next.x-current.x)};
+            newpoints.push_back(inpoint);
+            
+            newpoints.push_back(next);
+        }else if(isFirstInside && !isSecondIn){ // in->out
+            vec2 inpoint = {xmax,current.y+(xmax-current.x)*(next.y-current.y)/(next.x-current.x)};
+            newpoints.push_back(inpoint);
+        }
+    }
+    swap(newpoints,mypoly);newpoints.clear();//"move" mypoly = newpoints, clear newpoints
+    //l
+    n = mypoly.size();
+    for(int i=0;i<n;i++){
+        vec2 current = mypoly[i];
+        vec2 next = mypoly[(i+1)%n];
+
+        bool isFirstInside = current.x>=xmin,isSecondIn = next.x>=xmin;
+
+        if(isFirstInside&&isSecondIn){ //in->in
+            newpoints.push_back(next);
+        }else if(!isFirstInside && isSecondIn){ // out->in
+            vec2 inpoint = {xmin,current.y+(xmin-current.x)*(next.y-current.y)/(next.x-current.x)};
+            newpoints.push_back(inpoint);
+            
+            newpoints.push_back(next);
+        }else if(isFirstInside && !isSecondIn){ // in->out
+            vec2 inpoint = {xmin,current.y+(xmin-current.x)*(next.y-current.y)/(next.x-current.x)};  // x0=x0+ ymax-y1 * x1-x0
+            newpoints.push_back(inpoint);
+        }
+    }
+    swap(newpoints,mypoly);newpoints.clear();//"move" mypoly = newpoints, clear newpoints
+
+
+    glColor3f(1,1,0);
+    drawPoly(mypoly);
+    cout<<"Left with "<<mypoly.size();
+}
+// Implements Sutherlandï¿½Hodgman algorithm
+void display() {
+    init();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(xmin,ymin);
+    glVertex2f(xmax,ymin);
+    glVertex2f(xmax,ymax);
+    glVertex2f(xmin,ymax);
+    glEnd();
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    drawPoly(poly);  // original in green
+    // i and k are two consecutive indexes
+    int cs = clipper_ends.size();
+
+    // We pass the current array of vertices, it's size
+    // and the end points of the selected clipper line
+    clip(poly);
+    // clip(old_poly_points, poly_size, clipper_points[i][0],
+    //      clipper_points[i][1], clipper_points[k][0], clipper_points[k][1]);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    // drawPoly(old_poly_points, poly_size);
+    glFlush();
+}
+
+// Driver code
+int main(int argc, char* argv[]) {
+    int n;
+    cout << "Enter no. of vertices: \n";
+    cin >> n;
+    int t1, t2;
+    // org_poly_size = poly_size;
+    for (int i = 0; i < n; i++) {
+        cout << "Polygon Vertex:\n";
+        // scanf("%d%d", &old_poly_points[i][0], &old_poly_points[i][1]);
+        cin >> t1 >> t2;
+        vec2 p = {t1, t2};
+        poly.push_back(p);
+        // org_poly_points[i][0] = old_poly_points[i][0];
+        // org_poly_points[i][1] = old_poly_points[i][1];
+    }
+
+    cout << "ymin,ymax,xmin,xmax";
+    cin >> ymin >> ymax >> xmin >> xmax;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Polygon Clipping!");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
