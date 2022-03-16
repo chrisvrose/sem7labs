4c07aaecb0ccf6b667c3ce4008d553f527b45901
Christopher Rose <christophervrose@gmail.com>
2022-03-17 00:50:14 +0530

    finish 5

--- padp-f/src/50/main.c
+++ padp-f/src/50/main.c
@@ -1,13 +1,13 @@
 #include <mpi.h>
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 
-#define BS 32
+#define BS 20
 
-char m[3][20] = { "Hello\n", "RVCE\n", "CSE\n" };
+char m[3][BS] = {"Hello\n", "RVCE\n", "CSE\n"};
 // mpu
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     MPI_Status status;
     int rank, size;
@@ -15,13 +15,16 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (!rank) {
+    if (rank != 0) {
+        // for (int i = 1; i < size; i++) {
+        MPI_Send(m[((rank - 1) % 3)], BS, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        // }
+    } else {
         for (int i = 1; i < size; i++) {
-            MPI_Recv(msg, BS, MPI_CHAR,i , 0,MPI_COMM_WORLD,&status);
-            printf("Received %.32s is the msg from rank %d/%d\n",msg,rank,size);
+            MPI_Recv(msg, BS, MPI_CHAR, i, 0, MPI_COMM_WORLD,&status);
+            printf("Received %.20s is the msg from rank %d/%d\n", msg, i,
+                   size);
         }
-    }else{
-        MPI_Send(m[((rank-1)%3)],BS,MPI_CHAR,0,0,MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
